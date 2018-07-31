a32d0273b50d7ef8be9d6192e26d38d398e30ec0
Josias Martin <josiasmc@emypeople.net>
2018-07-30 19:40:43 -0600

    Fixed misnamed variables

--- src/Facturador.php
+++ src/Facturador.php
@@ -47,7 +47,7 @@ class Facturador
             'base_datos' => 'e_facturacion',
             'usuario' => 'root',
             'contra' => 'password',
-            'cryptoKey' => 
+            'llave' => 
                 'def0000057b1b0528f59f7ba3da8a25f60e9498bb0060'.
                 'a652843681d9f8ca53746679318aab2e54a9d4c2485f4'.
                 '6441709de9f0c4aa494dc31acf3d64484f88089296ebe6'
@@ -62,7 +62,7 @@ class Facturador
         );
         // Initialize the container
         $this->container = [
-            'cryptoKey' => Key::loadFromAsciiSafeString($config['cryptoKey']),
+            'cryptoKey' => Key::loadFromAsciiSafeString($config['llave']),
             'db' => $db
         ];
     }
